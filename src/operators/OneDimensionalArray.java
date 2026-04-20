d3362a8e047c2ec7e8b8234e69765d70b11c9f31
QAKRISHNA <krishnasoftwaretrainer@gmail.com>
2026-04-20 11:40:35 +0530

    New Eclipse Project New Pacakage

---
+++ src/operators/OneDimensionalArray.java
@@ -0,0 +1,10 @@
+package operators;
+
+public class OneDimensionalArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("It is OneDimensionalArray");
+	}
+
+}
